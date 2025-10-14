e05064c4df705c5eeaa08f6c2b31b57faeb7b741
Abhay Raj <rajabhay432000@gmail.com>
2025-10-15 03:13:09 +0530

    Modified galllery.jsx to have image corousel on clcking view button and added keyboard navigation functionality also

--- image-app/src/Pages/Gallery.jsx
+++ image-app/src/Pages/Gallery.jsx
@@ -3,8 +3,26 @@ import axios from "axios";
 
 export default function Gallery() {
   const [images, setImages] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
+  // const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
+  // this method will handle the view button functionality
+  const handleView = (index) => {
+    setSelectedIndex(index);
+  };
+  // this method handles the close icon functionality for corousel
+  const handleClose = () => {
+    setSelectedIndex(null);
+  };
+  // this  method will show next image in circular manner
+  const handleNext = () => {
+    setSelectedIndex((prev) => (prev < images.length - 1 ? prev + 1 : 0));
+  };
+
+  // this method will show prev image in reverse circular manner
+  const handlePrev = () => {
+    setSelectedIndex((prev) => (prev > 0 ? prev - 1 : images.length - 1));
+  };
   const fetchImages = async () => {
     try {
       const response = await axios.get(
@@ -31,15 +49,41 @@ export default function Gallery() {
     fetchImages();
   }, []);
 
+  // one more useEffect to handle keyboard navigation for corousel
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (selectedIndex === null) return;
+
+      switch (event.key) {
+        case "Escape":
+          handleClose();
+          break;
+        case "ArrowLeft":
+          handlePrev();
+          break;
+        case "ArrowRight":
+          handleNext();
+          break;
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [selectedIndex, images.length]);
+
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-4">Image Gallery</h2>
 
       <div className="grid grid-cols-3 gap-6">
         {images &&
-          images.map((image) => (
+          images.map((image, index) => (
             <div
-              key={image.Id}
+              key={image.id}
               className="p-4 border rounded shadow flex flex-col items-center"
             >
               <img
@@ -52,7 +96,7 @@ export default function Gallery() {
 
               <div className="flex gap-2 mt-2">
                 <button
-                  onClick={() => setSelectedImage(image)}
+                  onClick={() => handleView(index)}
                   className="px-3 py-1 bg-red-500 text-white rounded"
                 >
                   View
@@ -69,7 +113,7 @@ export default function Gallery() {
           ))}
       </div>
 
-      {selectedImage && (
+      {/* {selectedImage && (
         <div
           onClick={() => setSelectedImage(null)}
           className="fixed inset-0 bg-black bg-opacity-70  flex items-center justify-center"
@@ -78,6 +122,48 @@ export default function Gallery() {
             src={`http://localhost:5164/api/Image/getImage/${selectedImage.id}`}
             className="max-w-3xl max-h-[90%] rounded shadow-lg"
           />
+        </div>
+      )} */}
+
+      {/* Initially we were showing just image while clicking the view buttton but now we are going to implement corousel feature */}
+
+      {selectedIndex !== null && (
+        <div className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50">
+          {/* close button */}
+          <button
+            className="absolute top-4 right-4 text-white text-4xl font-bold hover:text-red-400"
+            aria-label="Close"
+            onClick={handleClose}
+          >
+            X
+          </button>
+
+          {/* left navigation button */}
+          <button
+            className="absolute left-6 text-white text-5xl font-bold hover:scale-110 transition"
+            aria-label="Previous Image"
+            onClick={handlePrev}
+          >
+            ←
+          </button>
+
+          {/* dispaly selected iamge or based or right or left arrow */}
+
+          <img
+            src={`http://localhost:5164/api/Image/getImage/${images[selectedIndex].id}`}
+            alt={images[selectedIndex].fileName}
+            className="max-h-[90vh] max-w-[90vw] rounded-lg shadow-lg"
+          />
+
+          {/* right navigation */}
+
+          <button
+            className="absolute right-6 text-white text-5xl font-bold hover:scale-110 transition"
+            aria-label="Next Image"
+            onClick={handleNext}
+          >
+            →
+          </button>
         </div>
       )}
     </div>
